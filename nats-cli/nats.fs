ef749c4a9aa1ffa1f66891876be8ffa365b41d15
Rob <rkayman@gmail.com>
2021-08-19 02:39:45 -0700

    Big refactor to condense code, leverage recursion

--- nats-cli/nats.fs
+++ nats-cli/nats.fs
@@ -2,196 +2,135 @@ namespace NATS
 
     open System
     open System.Buffers
+    open System.IO
     open System.IO.Pipelines
     open System.Net.Sockets
+    open System.Runtime.CompilerServices
     open System.Text 
     open System.Threading
     open System.Threading.Tasks
-    open FSharp.Control.Tasks.NonAffine
+    open FSharp.Control.Tasks.V2.ContextInsensitive
+    open FsToolkit.ErrorHandling
 
-    module Transport = 
+    
+    module Client =
+        
+        module Receiver = 
+            
+            let subscribe (stream: Stream) subject token =
+                task {
+                    let connId = Guid.NewGuid().ToString("N")
+                    let msg = $"SUB %s{subject} %s{connId}\r\n"
+                    let bytes = Encoding.UTF8.GetBytes(msg)
+                    let rom = ReadOnlyMemory(bytes)
+                    do! stream.WriteAsync(rom, token)
+                } :> Task
+                
+            [<MethodImpl(MethodImplOptions.AggressiveOptimization)>]
+            let write (stream: Stream) (writer: PipeWriter) token =
+                task {
+                    let mutable error = null
+                    try 
+                        try
+                            let writeMore = ref true
+                            while !writeMore do 
+                                let buffer = writer.GetMemory(1)
+                                let! bytesRead = stream.ReadAsync(buffer, token)
+                                writeMore := bytesRead <> 0
+                                if ! writeMore then
+                                    writer.Advance(bytesRead)
+                                    let! result = writer.FlushAsync(token)
+                                    writeMore := not result.IsCompleted && not result.IsCompleted
+                            
+                        with ex ->
+                            error <- ex
 
-        type DuplexPipe(input: PipeReader, output: PipeWriter) = 
-            interface IDuplexPipe with 
-                member this.Input with get() = input 
-                member this.Output with get() = output 
+                    finally
+                        writer.Complete(error)
+                }
+                
+            let private lineFound = Event<string>()
+            let Lines = lineFound.Publish
+            
+            let private NewLine = byte('\n')
+            
+            [<MethodImpl(MethodImplOptions.AggressiveInlining)>]
+            let private getNextNewLinePosition buf = buf.PositionOf(NewLine) |> Option.ofNullable
+            
+            [<MethodImpl(MethodImplOptions.AggressiveOptimization)>]
+            let rec private processBuffer (buf: ReadOnlySequence<byte>) =
+                match getNextNewLinePosition buf with
+                | None -> buf
+                | Some pos ->
+                    let seq = buf.Slice(0, pos)
+                    let line = Encoding.UTF8.GetString(&seq)
+                    lineFound.Trigger(line)
+                    buf.Slice(buf.GetPosition(1L, pos)) |> processBuffer
 
-        let createConnectionPair inputOptions outputOptions = 
-            let input  = Pipe(inputOptions)
-            let output = Pipe(outputOptions)
-
-            let transportToApplication = DuplexPipe(output.Reader, input.Writer) 
-            let applicationToTransport = DuplexPipe(input.Reader, output.Writer) 
-
-            struct {| Transport = applicationToTransport; Application = transportToApplication|} 
-
-    module Client = 
-        exception NATSConnectionException of string 
-
-        let openTcp (url: Uri) = 
+            [<MethodImpl(MethodImplOptions.AggressiveOptimization)>] 
+            let read (reader: PipeReader) token =
+                task {
+                    let mutable error = null
+                    try 
+                        try
+                            let buf = ref Unchecked.defaultof<ReadOnlySequence<byte>>
+                            let readMore = ref true
+                            while !readMore do
+                                let! result = reader.ReadAsync(token)
+                                readMore := not result.IsCompleted && not result.IsCanceled
+                                if not result.Buffer.IsEmpty then
+                                    buf := processBuffer result.Buffer 
+                                    reader.AdvanceTo((!buf).Start, (!buf).End)
+                        with ex -> 
+                            error <- ex 
+                    finally
+                        reader.Complete(error)
+                } :> Task
+ 
+        
+        exception NATSConnectionException of string
+        
+        let private openTcp (url: Uri) = 
             task {
-                if url.Scheme.ToLowerInvariant() <> "nats" then raise (NATSConnectionException $"Unable to connect to unknown scheme ({url.Scheme})")
+                if url.Scheme.ToLowerInvariant() <> "nats" then
+                    raise (NATSConnectionException
+                               $"Expected NATS scheme.  Unable to connect to unknown scheme ({url.Scheme})")
 
                 let addressFamily, dualMode = if Socket.OSSupportsIPv6 
                                               then AddressFamily.InterNetworkV6, true 
                                               else AddressFamily.InterNetwork, false 
 
                 let client = new TcpClient(addressFamily) 
-                client.Client.DualMode <- dualMode 
+                client.Client.DualMode <- dualMode
+                client.ReceiveBufferSize <- 64 * 1024
+                client.SendBufferSize <- 32 * 1024 
 
                 let server = if url.IsDefaultPort then UriBuilder(url.Scheme, url.Host, 4222).Uri else url 
 
                 use cancel = new CancellationTokenSource(2000)
-                do! client.ConnectAsync(server.Host, server.Port, cancel.Token) 
-                if cancel.IsCancellationRequested then raise (NATSConnectionException "timeout")
-                client.NoDelay <- false 
-                client.ReceiveBufferSize <- 64 * 1024 
-                client.SendBufferSize <- 32 * 1024 
+                do! client.ConnectAsync(server.Host, server.Port, cancel.Token)
                 return client
             }
 
-        let trueTask = ValueTask.FromResult(true)
-        let falseTask = ValueTask.FromResult(false) 
-
-        let subscribe (stream: NetworkStream) token = 
+        let listen onUrl forSubject cancel =
             task {
-                let msg = "SUB > 100\r\n" 
-                let bytes = Encoding.UTF8.GetBytes(msg) 
-                let rom = ReadOnlyMemory(bytes)
-                do! stream.WriteAsync(rom, token) 
-            }
-
-        let writeLoopRec (client: TcpClient) (pipe: Pipe) token = 
-            task {
-                let stream = client.GetStream() 
-                do! subscribe stream token 
-
-                let rec writeMore (stream: NetworkStream) (writer: PipeWriter) token = 
-                    task {
-                        try 
-                            let buffer = writer.GetMemory(1) 
-                            match! stream.ReadAsync(buffer, token) with 
-                            | 0 -> return! falseTask 
-                            | bytesRead -> 
-                                writer.Advance(bytesRead) 
-                                match! writer.FlushAsync(token) with 
-                                | x when x.IsCanceled || x.IsCompleted -> return! falseTask 
-                                | _ -> return! writeMore stream writer token 
-
-                        with ex -> 
-                            do! writer.CompleteAsync(ex) 
-                            return! trueTask 
-                    }
-                let! isComplete = writeMore stream pipe.Writer token 
-                if not isComplete then do! pipe.Writer.CompleteAsync() 
-            }
-
-        let writeLoop (client: TcpClient) (pipe: Pipe) token = 
-            task {
-                let stream = client.GetStream() 
-                do! subscribe stream token 
-
-                let writer = pipe.Writer 
-                let mutable error = null 
+                let pipeOptions = PipeOptions(readerScheduler = PipeScheduler.Inline,
+                                              writerScheduler = PipeScheduler.Inline,
+                                              useSynchronizationContext = false)
+                let receivePipe = Pipe(pipeOptions)
+                
                 try 
-                    let mutable more = true 
-                    while more do 
-                        let buffer = writer.GetMemory(1) 
-                        let! bytesRead = stream.ReadAsync(buffer, token) 
-                        if bytesRead = 0 then more <- false 
-                        else 
-                            writer.Advance(bytesRead) 
-                            let! flush = writer.FlushAsync(token) 
-                            if flush.IsCanceled || flush.IsCompleted then more <- false 
+                    let! client = openTcp onUrl
+                    let stream = client.GetStream()
                     
-                with ex -> 
-                    error <- ex
-
-                do! writer.CompleteAsync(error) 
-            }
-
-        let readLoopRec (pipe: Pipe) token processLine = 
-            task { 
-                let rec processBuffer buf = 
-                    match buf.PositionOf(byte('\n')) |> Option.ofNullable with 
-                    | None -> buf
-                    | Some pos -> 
-                        processLine(buf.Slice(0, pos)) 
-                        processBuffer (buf.Slice(buf.GetPosition(1L, pos))) 
-
-                let rec readMore (reader: PipeReader) token = 
-                    task {
-                        try 
-                            match! reader.ReadAsync(token) with 
-                            | x when x.IsCompleted || x.IsCanceled -> return! falseTask 
-                            | result -> 
-                                let buffer = processBuffer result.Buffer 
-                                reader.AdvanceTo(buffer.Start, buffer.End)
-                                return! readMore reader token
-
-                        with ex -> 
-                            do! reader.CompleteAsync(ex) 
-                            return! trueTask 
-                    }
-                let! isComplete = readMore pipe.Reader token 
-                if not isComplete then do! pipe.Reader.CompleteAsync() 
-            }
-
-        let readLoopRecInner (pipe: Pipe) token processLine = 
-            task { 
-                let reader = pipe.Reader 
-
-                let mutable error = null
-                try 
-                    let mutable readMore = true 
-                    while readMore do 
-                        let! result = reader.ReadAsync(token) 
-
-                        let rec processBuffer buf = 
-                            match buf.PositionOf(byte('\n')) |> Option.ofNullable with 
-                            | None -> buf
-                            | Some pos -> 
-                                processLine(buf.Slice(0, pos)) 
-                                processBuffer (buf.Slice(buf.GetPosition(1L, pos))) 
-                        let buffer = processBuffer result.Buffer 
-
-                        reader.AdvanceTo(buffer.Start, buffer.End) 
-
-                        readMore <- not (result.IsCompleted || result.IsCanceled)
-
-                with ex -> 
-                    error <- ex 
-
-                do! reader.CompleteAsync(error)
-            }
-
-        let readLoop (pipe: Pipe) token processLine = 
-            task { 
-                let reader = pipe.Reader 
-
-                let mutable error = null
-                try 
-                    let mutable readMore = true 
-                    while readMore do 
-                        let! result = reader.ReadAsync(token) 
-
-                        let mutable buffer = result.Buffer 
-                        let mutable processMore = true 
-                        while processMore do 
-                            let position = buffer.PositionOf(byte('\n'))
-                            if not position.HasValue 
-                            then processMore <- false 
-                            else 
-                                processLine(buffer.Slice(0, position.Value)) 
-                                buffer <- buffer.Slice(buffer.GetPosition(1L, position.Value)) 
-
-                        reader.AdvanceTo(buffer.Start, buffer.End) 
-
-                        readMore <- not (result.IsCompleted || result.IsCanceled)
-
-                with ex -> 
-                    error <- ex 
-
-                do! reader.CompleteAsync(error)
-            }
-
+                    do! Receiver.subscribe stream forSubject cancel
+                    let outputTask = Receiver.write stream receivePipe.Writer cancel
+                    let inputTask  = Receiver.read receivePipe.Reader cancel 
+                    
+                    do! Task.WhenAll(outputTask, inputTask)
+                    
+                with ex ->
+                    eprintfn $"{ex.ToString()}"
+                    
+                return ()
+            } :> Task
